81d8062d8a6b3177f8051a65713439710d287e4b
Hassan <hskuntze@gmail.com>
2021-02-04 07:52:10 -0300

    Project created

---
+++ src/test/java/com/KuntzeProjects/course2/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KuntzeProjects.course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
