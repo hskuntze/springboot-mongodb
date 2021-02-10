f9062f5742318984ba3c787588689512aa46bfbb
Hassan <hskuntze@gmail.com>
2021-02-09 21:14:16 -0300

    creating findById() and handling it's exceptions

--- src/main/java/com/KuntzeProjects/course2/services/UserService.java
+++ src/main/java/com/KuntzeProjects/course2/services/UserService.java
@@ -1,12 +1,14 @@
 package com.KuntzeProjects.course2.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.KuntzeProjects.course2.domain.User;
 import com.KuntzeProjects.course2.repository.UserRepository;
+import com.KuntzeProjects.course2.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> u = repository.findById(id);
+		return u.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
 	}
 }
