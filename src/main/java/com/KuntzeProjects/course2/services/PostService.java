34c084f0a51140d0cf02c363363c22acb233309c
Hassan <hskuntze@gmail.com>
2021-02-12 20:43:59 -0300

    findById and findAll - Post

---
+++ src/main/java/com/KuntzeProjects/course2/services/PostService.java
@@ -0,0 +1,25 @@
+package com.KuntzeProjects.course2.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.KuntzeProjects.course2.domain.Post;
+import com.KuntzeProjects.course2.repository.PostRepository;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> p = repository.findById(id);
+		return p.get();
+	}
+	
+	public List<Post> findAll(){
+		return repository.findAll();
+	}
+}
