e1aa15a64febac031f5745adad3e674060c760b7
Hassan <hskuntze@gmail.com>
2021-02-13 22:21:21 -0300

    Search with multiple parameters

--- src/main/java/com/KuntzeProjects/course2/repository/PostRepository.java
+++ src/main/java/com/KuntzeProjects/course2/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.KuntzeProjects.course2.repository;
 
 import java.util.List;
+import java.util.Date;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.KuntzeProjects.course2.domain.Post;
@@ -11,4 +13,10 @@ import com.KuntzeProjects.course2.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	@Query(" { 'title': { $regex: ?0, $options: 'i' } } ")
+	List<Post> searchTitle(String text);
+	
+	@Query("{ $and : [ {date: {$gte:?1}} , {date: {$lte:?2}} , {$or:[{'title': { $regex: ?0, $options: 'i' }} , {'body': { $regex: ?0, $options: 'i' }}, {'comments.text': { $regex: ?0, $options: 'i' }}]} ] }")
+	List<Post> completeSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/KuntzeProjects/course2/services/PostService.java
+++ src/main/java/com/KuntzeProjects/course2/services/PostService.java
@@ -1,5 +1,6 @@
 package com.KuntzeProjects.course2.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,11 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repository.findByTitleContainingIgnoreCase(text);
+		return repository.searchTitle(text);
+	}
+	
+	public List<Post> completeSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repository.completeSearch(text, minDate, maxDate);
 	}
 }
